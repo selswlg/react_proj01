b787d6bc6b983ccbc72be31810147334cc3a8a3f
JH_Lee <selswlhhh@gmail.com>
2022-05-13 20:44:27 +0900

    container and head complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import { useState, useEffect, useRef, useMemo } from "react";
+import { createGlobalStyle } from "styled-components";
+import TodoHead from "./components/TodoHead";
+import TodoTemplate from "./components/TodoTemplate";
+// global 적용 css
+const GlobalStyle = createGlobalStyle`
+  body {
+      background: #e9ecef;
+  }
+`;
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+
+      <TodoTemplate>안녕하세요!</TodoTemplate>
+    </>
+  );
 }
